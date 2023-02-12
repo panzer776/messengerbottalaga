6e1bcd44cc13f4cfe6dadeca415e4762c2b8bf82
unknown <campuuus7@gmail.com>
2023-02-12 16:26:58 +0800

    upgraded the bot

--- test.js
+++ test.js
@@ -1,3 +1,27 @@
-const play = require('play-dl');
+const request = require("request")
+const fs = require("fs")
+var link = "https://i.imgur.com/"
+var alphabet = "abcdefghijklmnopqrstuvwxyz123456789"
+var path = ""
+_()
+async function _() {
 
-play.video_info("Xb85Sz28S8Q").then(r=>console.log(r.format))
+    for (i = 0; i < 5; i++) {
+        path = path.concat(Math.round(Math.random()) ? alphabet[Math.round(Math.random() * 34)] : alphabet[Math.round(Math.random() * 34)].toUpperCase())
+    }
+        r = await request(link + path + ".jpg")
+
+        f = await r.pipe(fs.createWriteStream("./random.jpg"))
+        f.on("close", () => {
+            console.log(fs.statSync("./random.jpg").size / 1000 < 8)
+            if (fs.statSync("./random.jpg").size / 1000 < 8) {
+                path = ""
+                _()
+            } else {
+                console.log(link + path + ".jpg")
+            }
+    
+    })
+}
+
+
