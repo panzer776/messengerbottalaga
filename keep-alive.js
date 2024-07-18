3042d7f325c1887ba8344f6cbb5625ced6b4e060
unknown <campuuus7@gmail.com>
2024-07-18 18:38:38 +0800

    fixed keep alive

--- keep-alive.js
+++ keep-alive.js
@@ -1,7 +1,16 @@
 var http = require('http');
+const { send } = require('q');
 
 //create a server object:
 http.createServer(function (req, res) {
   res.write('buhay sha'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
+
+
+async function sendSignal(){
+    await new Promise(r=>setTimeout(r,30000))
+axios({method:"GET",url:"https://messengerbottalaga.onrender.com/"}).catch(err=>{console.log(err)})
+sendSignal()
+}
+sendSignal()
